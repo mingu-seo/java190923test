43b922eb996beb1f800f79464531885d9656e2f8
webshjin <web.shjin@gmail.com>
2019-12-26 16:39:57 +0900

    수정1

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("스프링");
+		String msg = g.greet("Git");
 		System.out.println(msg);
 		ctx.close();
 	}
