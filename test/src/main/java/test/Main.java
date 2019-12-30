1568715286f46594a87c2e1b94a9260d2870fc68
HSK <HSK@DESKTOP-FIO7IQT>
2019-12-30 11:20:39 +0900

    sujin

--- test/src/main/java/test/Main.java
+++ test/src/main/java/test/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(AppContext.class);
 		Greeter g = ctx.getBean("greeter", Greeter.class);
-		String msg = g.greet("Git");
+		String msg = g.greet("Git222");
 		System.out.println(msg);
 		ctx.close();
 	}
